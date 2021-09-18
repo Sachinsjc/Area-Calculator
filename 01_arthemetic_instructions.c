b92b1eeb6d91cc4cbdce93188744afb9c2649cc9
Sachin Chauhan <sachinsjc151@gmail.com>
2021-09-18 09:26:41 +0530

    Add files via upload

---
+++ 01_arthemetic_instructions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2;
+    int b = 8;
+    int c = 10;
+
+    printf("The value for a+b+c is %d\n", a + b + c);
+    printf("The value for a+b-c is %d\n", a + b - c);
+    printf("The value for a*b*c is %d\n", a * b * c);
+    printf("The value for a+b/c is %d\n", a + b / c);
+    return 0;
+}
